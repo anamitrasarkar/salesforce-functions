be3eb9d9846aa7cf42be2506b71bbf7aa0635504
Anamitra Sarkar <anamitra.sarkar@anamitrasa-ltm2.internal.salesforce.com>
2022-03-19 17:39:01 -0400

    Added tfjs in myfunction

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -1,3 +1,5 @@
+"use strict"
+import * as toxicity from '@tensorflow-models/toxicity';
 /**
  * Describe Myfunction here.
  *
@@ -13,7 +15,28 @@
  
 export default async function (event, context, logger) {
     logger.info(`Invoking Myfunction with payload ${JSON.stringify(event.data || {})}`);
-    const results = await context.org.dataApi.query('SELECT Id, Name FROM Account');
-    logger.info(JSON.stringify(results));
-    return results;
+
+    // The minimum prediction confidence.
+    const threshold = 0.9;
+    // Which toxicity labels to return.
+    const labelsToInclude = ['identity_attack', 'insult', 'threat'];    
+    toxicity.load(threshold, labelsToInclude)
+        .then(model => {
+            // Now you can use the `model` object to label sentences. 
+            model.classify(['you suck'])
+                .then(predictions => {
+                    logger.info(JSON.stringify(predictions))
+                    return JSON.stringify(predictions)            
+                })
+                .catch(error => {
+                    logger.info('error during classify' + error)
+                })
+        })    
+        .catch(error => {
+            logger.info('error during model load ' + error)
+        })
+
+    // const results = await context.org.dataApi.query('SELECT Id, Name FROM Account');
+    // logger.info(JSON.stringify(results));
+    // return results;
 }
